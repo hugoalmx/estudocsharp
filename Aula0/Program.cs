07d6afcd77683b75adde5c2ae1e6cdb6f11c8dfd
Hugo Almeida <hugovieirajp@hotmail.com>
2025-07-03 12:01:29 -0300

    Repositorio para estudo primeiro commit

---
+++ Aula0/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Olá Mundo!");
+Console.Write("A data de hoje é : " + DateTime.Now);
+
+
+// O comando DateTime.Now retorna a data e hora atual do sistema 
+// O comando Console.WriteLine pode receber uma string ou um objeto como parâmetro
+// O comando Console.WriteLine imprime a mensagem no console e pula uma linha após a mensagem
+// O comando Console.Write imprime a mensagem no console sem pular uma linha após a mensagem
+
+
---
+++ Aula01/Program.cs
@@ -0,0 +1,25 @@
+﻿//Exercício - Imprimir valores literais
+
+//Um valor literal é um valor constante que nunca é alterado. Anteriormente, você exibia uma cadeia de caracteres literal no console de saída. Em outras palavras, você literalmente queria que a sequência de caracteres alfanuméricos H, e, l, l, o e assim por diante fosse exibida no console de saída.
+//Use o tipo de dados de cadeia de caracteres sempre que você tiver palavras alfanuméricas, frases ou dados para apresentação, não cálculo. Que outros tipos de dados literais você pode imprimir na saída?
+
+//CHAR LITERAL = Pode-se utilizar as aspas simples para definir um caractere literal. 
+//Por exemplo, 'A' é um caractere literal que representa a letra A.
+
+Console.WriteLine('H'); //Exemplo de caractere literal
+
+//STRING LITERAL = Pode-se utilizar as aspas duplas para definir uma cadeia de caracteres literal. 
+//Por exemplo, "Olá Mundo!" é uma cadeia de caracteres literal que representa a frase Olá Mundo!.
+
+Console.WriteLine("Olá Mundo!"); //Exemplo de cadeia de caracteres literal
+
+//Para valor numérico, você pode usar o tipo de dados inteiro (int) ou
+// ponto flutuante float(6-9 digitos), double(15-17 digitos) e decimal(28-29 digitos) para representar números literais.
+
+Console.WriteLine(42); // Exemplo de valor numérico inteiro
+
+
+Console.WriteLine(3.14); // Exemplo de valor numérico ponto flutuante
+
+Console.WriteLine(0.25F); // Exemplo de valor numérico ponto flutuante com float especificado com 'F' ou 'f'
+
